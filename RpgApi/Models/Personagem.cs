9c5c94177f1ae3a46936aa3c9f53443c4d6d70fb
jjmonstro <jpcorreia1211@gmail.com>
2025-02-10 08:37:54 -0300

    creating the base api project

---
+++ RpgApi/Models/Personagem.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RpgApi.Models
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int PontosVida { get; set; }
+        public int Forca { get; set; }
+        public int Defesa { get; set; }
+        public int Inteligencia { get; set; }
+    }
+}
