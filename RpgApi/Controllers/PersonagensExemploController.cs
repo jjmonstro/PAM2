1e3172e30273e35a4eaeced7ff4f331ff59efb7e
jjmonstro <jpcorreia1211@gmail.com>
2025-02-25 17:46:08 -0300

    Resolvendo conflito de merge

--- RpgApi/Controllers/PersonagensExemploController.cs
+++ RpgApi/Controllers/PersonagensExemploController.cs
@@ -25,65 +25,45 @@ namespace RpgApi.Controllers
             new Personagem() { Id = 7, Nome = "Radagast", PontosVida=100, Forca=25, Defesa=11, Inteligencia=35, Classe=ClasseEnum.Mago }
         };
 
-<<<<<<< HEAD
         [HttpGet("Get")]        
-=======
-        [HttpGet("Get")]
->>>>>>> 77e5c1c74e71c7e641c43ed248fd220e723dda2a
         public IActionResult GetFirst()
         {
             Personagem p = personagens[0];
             return Ok(p);
         }
 
-<<<<<<< HEAD
         [HttpGet("Getall")]
-=======
-        [HttpGet("GetAll")]
->>>>>>> 77e5c1c74e71c7e641c43ed248fd220e723dda2a
         public IActionResult Get()
         {
             return Ok(personagens);
         }
 
         [HttpPost]
-<<<<<<< HEAD
         public IActionResult AddPersonagem(Personagem novoPersonagem)
         {
             personagens.Add(novoPersonagem);
             return Ok(personagens);
         }
+
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
             personagens.RemoveAll(pers => pers.Id == id);
-=======
-        public IActionResult AddPersonagem(Personagem novopersonagem)
-        {
-            personagens.Add(novopersonagem);
->>>>>>> 77e5c1c74e71c7e641c43ed248fd220e723dda2a
-            return Ok(personagens);
         }
 
         [HttpPut]
-<<<<<<< HEAD
-        public IActionResult UpdatePersonagem(Personagem p)
-        {
-            Personagem personagemAlterado = personagens.Find(pers => pers.Id == p.Id);
-=======
         public IActionResult UpdatePersonagem(Personagem p){
             Personagem personagemAlterado = personagens.Find(pers=> pers.Id == p.Id);
->>>>>>> 77e5c1c74e71c7e641c43ed248fd220e723dda2a
             personagemAlterado.Nome = p.Nome;
             personagemAlterado.PontosVida = p.PontosVida;
             personagemAlterado.Forca = p.Forca;
             personagemAlterado.Defesa = p.Defesa;
             personagemAlterado.Inteligencia = p.Inteligencia;
             personagemAlterado.Classe = p.Classe;
-<<<<<<< HEAD
             
             return Ok(personagens);
         }
+        
 
         [HttpGet("GetByEnum/{enumID}")]
         public IActionResult GetByEnum(int enumID)
@@ -94,30 +74,11 @@ namespace RpgApi.Controllers
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetSingle(int id){
+        public IActionResult GetSingle(int id)
+        {
             return Ok(personagens.FirstOrDefault(pe => pe.Id == id));
         }
 
-        
-=======
-
-            return Ok(personagens);
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id){
-            personagens.RemoveAll(pers => pers.Id == id);
-            return Ok(personagens);
-        }
-
-        [HttpGet("GetByEnum/{enumId}")]
-        public IActionResult GetByEnum(int enumId){
-            ClasseEnum enumDigitado = (ClasseEnum)enumId;
-
-            List<Personagem> listaBusca = personagens.FindAll(p => p.Classe == enumDigitado);
-
-            return Ok(listaBusca);
-        }
->>>>>>> 77e5c1c74e71c7e641c43ed248fd220e723dda2a
+       
     }
 }
