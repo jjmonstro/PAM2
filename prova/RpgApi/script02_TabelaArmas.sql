aba808fd81fe4cac93a7b32bc9e916ee246bd7e9
jjmonstro <jpcorreia1211@gmail.com>
2025-03-24 08:41:12 -0300

    exam

---
+++ prova/RpgApi/script02_TabelaArmas.sql
@@ -0,0 +1,27 @@
+﻿BEGIN TRANSACTION;
+CREATE TABLE [TB_ARMAS] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] Varchar(200) NOT NULL,
+    [Dano] int NOT NULL,
+    CONSTRAINT [PK_TB_ARMAS] PRIMARY KEY ([Id])
+);
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_ARMAS]'))
+    SET IDENTITY_INSERT [TB_ARMAS] ON;
+INSERT INTO [TB_ARMAS] ([Id], [Dano], [Nome])
+VALUES (1, 5, 'Espada de Madeira'),
+(2, 10, 'Espada de Ferro'),
+(3, 15, 'Espada de Diamante'),
+(4, 20, 'Cajado de Fogo'),
+(5, 25, 'Cajado de Gelo'),
+(6, 35, 'Cajado de Relâmpago'),
+(7, 999, 'Lança');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_ARMAS]'))
+    SET IDENTITY_INSERT [TB_ARMAS] OFF;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250324111542_MigracaoArma', N'9.0.2');
+
+COMMIT;
+GO
+
