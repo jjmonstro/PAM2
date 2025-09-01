0b84362c1b222cc1349e7fb173e7640612ee200a
jjmonstro <jpcorreia1211@gmail.com>
2025-09-01 08:46:44 -0300

    styles

--- RpgApiAPP/AppRpgEtec/MainPage.xaml.cs
+++ RpgApiAPP/AppRpgEtec/MainPage.xaml.cs
@@ -9,17 +9,19 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
+        
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count++;
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        //    if (count == 1)
+        //        CounterBtn.Text = $"Clicked {count} time";
+        //    else
+        //        CounterBtn.Text = $"Clicked {count} times";
+
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
     }
 
 }
